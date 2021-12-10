1b033e5e8ea4643184cee8a3187db1862fc42644
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-12-10 19:33:00 +0530

    chore: :seedling: Removed post controller and finished GET functions

--- api/controllers/projectsController.js
+++ api/controllers/projectsController.js
@@ -1,50 +1,30 @@
-
-const fs = require('fs');
-const path = require('path');
-const data = require(path.join(
-  __dirname,
-  '../dev-data/data/projects.json'
-))
+const Project = require('../models/projectModel.js');
+const ApiFeatures = require('../utils/apiFeatures.js');
+const catchAsync = require('../utils/catchAsync');
 
 //Exporting the functions to be executed for specific route
-exports.getAllProjects = (req, res, next) => {
+exports.getAllProjects = catchAsync(async (req, res, next) => {
+
+	const feature = new ApiFeatures(Project.find(), req.query).sort().limitFields().pagination();
+
+	const projects = await feature.query;
 	
-	try {
-		res.status(200).json({
-			status: 'successs',
-			result: data.length,
-			data
-		})
-	} catch(err) {
-		res.status(400).json({
-			status: 'Error',
-			message: 'Something went wrong please try again later',
-			error: {
-				err
-			}
-		});
-	}
-};
 
-exports.getSingleProject = (req, res, next) => {
-	try {
-		const id = req.params.id*1;
+	res.status(200).json({
+		status: 'successs',
+		result: projects.length,
+		projects
+	})
+	
+});
 
-		const projectData = data.filter((ele) => {
-			return ele.id === id;
-		})
+exports.getSingleProject = catchAsync (async (req, res, next) => {
+	
+	const project = await Project.findById(req.params.id);
 
-		res.status(200).json({
-			status: 'success',
-			projectData
-		})
-	} catch(err) {
-		res.status(400).json({
-			status: 'Error',
-			message: 'Something went wrong please try again later',
-			error: {
-				err
-			}
-		});
-	}
-}
+	res.status(200).json({
+		status: 'success',
+		project
+	});
+	
+});
