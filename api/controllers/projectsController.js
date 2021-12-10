11050141198ae2c7488d7a0f220ff2b8bc8ae7aa
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-12-10 15:10:05 +0530

    feat: :rocket: Added route for getting single project (Fetching data from static file)

--- api/controllers/projectsController.js
+++ api/controllers/projectsController.js
@@ -25,3 +25,26 @@ exports.getAllProjects = (req, res, next) => {
 		});
 	}
 };
+
+exports.getSingleProject = (req, res, next) => {
+	try {
+		const id = req.params.id*1;
+
+		const projectData = data.filter((ele) => {
+			return ele.id === id;
+		})
+
+		res.status(200).json({
+			status: 'success',
+			projectData
+		})
+	} catch(err) {
+		res.status(400).json({
+			status: 'Error',
+			message: 'Something went wrong please try again later',
+			error: {
+				err
+			}
+		});
+	}
+}
--- api/routes/projectRoutes.js
+++ api/routes/projectRoutes.js
@@ -9,5 +9,7 @@ const route = express.Router();
 
 route.route('/').get(projectController.getAllProjects);
 
+route.route('/:id').get(projectController.getSingleProject);
+
 //Exporting the routes
 module.exports = route;
