24bd775b644367d0992ece6bbd78ceb1899b44ef
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-12-10 19:30:38 +0530

    feat: :fire: Added global error handling middleware

--- api/index.js
+++ api/index.js
@@ -1,13 +1,22 @@
 //External Packages import
 const express = require('express');
+const path = require('path');
 
 //Internal Files and module imports
 const projectRoutes = require('./routes/projectRoutes');
+const globalErrorHandler = require(path.join(__dirname,'./controllers/errorController.js'));
+const appError = require(path.join(__dirname, './utils/appError.js'));
 
 //Assigining and middleware hooks
 const app = express();
 
 app.use('/api/v1/projects', projectRoutes);
+
+app.all('*', (req, res, next) => {
+  next(new appError(`${req.originalUrl} was not found on the server. Please check the Url :D`));
+});
+
+app.use(globalErrorHandler);
 
 //Exporting the module
 module.exports = app;
