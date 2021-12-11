77989a8c917c28c13fb99ec29d08d81f6782cf03
Ashwin Kumar Uppala <53075480+ashwinexe@users.noreply.github.com>
2021-12-11 23:35:48 +0530

    fetched main

---
+++ SECURITY.md
@@ -0,0 +1,9 @@
+# Security Policy
+
+## Reporting a Vulnerability
+
+In case you find any security vulnerability then do not raise an issue. Kindly report it on admin emails below
+- [Kaiwalya Koparkar](mailto:kaiwalyakoparkar@gmail.com)
+- [Karuna Tata](mailto:karunatata63@gmail.com)
+- [Ashwin Kumar Uppala](mailto:kumarashwin2603@gmail.com)
+- [Abhishek Choudhary](mailto:shreemaan.abhishek@gmail.com)
--- api/server.js
+++ api/server.js
@@ -3,6 +3,10 @@ const chalk = require('chalk');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 const app  = require('./index.js');
 
 const port = process.env.PORT || 3000;
@@ -20,12 +24,20 @@ mongoose.connect(DB, {
 	log(chalk.cyan('💽 Database Connected Successfully'));
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	log(chalk.cyan(`🏃 Server started at http://localhost:${port}`));
 });
 
 function serverDownAlerts() {
 	//Twillio function here
+
+    client.messages
+      .create({
+         body: 'App crashing!!',
+         from: '+15017122661',
+         to: process.env.DEST_NUMBER
+       })
+      .then(message => console.log(message.sid));
 }
 
 process.on('unhandledRejection', err => {
