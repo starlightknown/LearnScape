0c607e1ee533c75e8d26f2a7049b6eea2e8c0045
Ashwin Kumar Uppala <53075480+ashwinexe@users.noreply.github.com>
2021-12-11 23:30:57 +0530

    ✅Add React frontend

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <p>Hello Wolrd</p>
+            <h1>Helloo</h1>
+        </div>
+        
+    );
+}
