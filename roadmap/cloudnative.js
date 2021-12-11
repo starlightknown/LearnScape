ce2c41e88f0303ea2806c6364df5f53d69427781
starlightknown <karunatata63@gmail.com>
2021-12-11 08:40:38 +0530

    feat: :white_check_mark: remove template text

--- roadmap/cloudnative.js
+++ roadmap/cloudnative.js
@@ -1,6 +1,8 @@
 // Use this sample to create your own voice commands
+visualHints('Say "what is meant by devops?"',
+           'Say "hey sky"');
 intent('(hello|hi|hey there|hey|hey sky)', p => {
-    p.play('(Hi, this is Sky, your LearnScape voice assistant!)');
+    p.play('(Hi, this is Sky, your LearnScape voice assistant!I will try my best to answer your questions about DevOps)');
 });
 intent('What is Meant by DevOps?', p => {
     p.play('(The term DevOps is simply dev from development + ops from operations. So it is about combining the development of an application or service with the operations tasks of supporting it.)');
@@ -14,3 +16,37 @@ intent('(Is DevOps Hard?|Is DevOps Easy to Learn?|Is DevOps Technical?)', p => {
 intent('What is DevOps AWS?', p => {
     p.play('(AWS – Amazon Web Services – is a cloud computing service provider. One of the biggest and most successful, delivering full function software as a service platform. It runs things for you, so you don’t have to.)');
 });
+intent('is there a roadmap?','how to get started?','what is the way?','any guidance to start?','where to start?', p => {
+    p.play('(Would you like to see the roadmap or projects to contribute?)');
+    p.then(showprojects);
+});
+
+let showprojects = context(() => {
+    follow("(probably|)$(ANSWER yes|sure|no)", p => {
+        let answer = p.ANSWER.value;
+        if (['yes', 'sure'].indexOf(answer) >= 0) {
+            p.play({command: 'show-projects'}); 
+            p.play("let's get you started");
+            p.play({command: 'highlightItem', data:{item: 'programminglang'}});
+            p.play("choose your programming language such as python, go, rust, ruby, node js");
+            p.play({command: 'highlightItem', data:{item: 'linuxconcepts'}});
+            p.play("Understanding of Linux & OS Concepts");
+            p.play({command: 'highlightItem', data:{item: 'deployment'}});
+            p.play("Knowledge of Software Configuration & Deployment");
+            p.play({command: 'highlightItem', data:{item: 'infrastructure'}});
+            p.play(" Infrastructure as Code lets you manage your IT infrastructure using the configuration files such as Azure Resource Manager, Terraform, SaltStack");
+            p.play({command: 'highlightItem', data:{item: 'ci'}});
+            p.play("The understanding of Continuous Integration/Continuous Delivery (CI/CD) such as Jenkins, CircleCI, GitLab");
+            p.play({command: 'highlightItem', data:{item: 'networking'}});
+            p.play("have a knowledge of networking concepts to manage the IT workflow. ");
+            p.play({command: 'highlightItem', data:{item: 'tools'}});
+            p.play("you’re required to get yourself armed with various DevOps tools like Git, Docker, Chef, Puppet, Ansible, etc. Also, it is very crucial for the DevOps professional to collect the feedbacks and implement the changes at the earliest.");
+            p.play({command: 'highlightItem', data:{item: 'cloud'}});
+            p.play(" it becomes necessary for DevOps professionals to learn about Cloud Services and Providers");
+        } else if (answer === 'no') {
+            p.play("we look forward to help you next time!")
+        }
+    })
+});
+
+
